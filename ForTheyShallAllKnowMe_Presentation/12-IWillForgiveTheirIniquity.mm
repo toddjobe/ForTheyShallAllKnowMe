af9a2d8ecd4b018f006d046b95f9b03eca93c926
unknown <todd.jobe@GLDLBAE446035.goldlnk.rootlnka.net>
2016-05-29 08:10:37 -0400

    last lesson

---
+++ ForTheyShallAllKnowMe_Presentation/12-IWillForgiveTheirIniquity.mm
@@ -0,0 +1,55 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1463911890590" ID="ID_162655984" MODIFIED="1463911902571" TEXT="I Will Forgive Their Iniquity">
+<node CREATED="1463911906245" ID="ID_1219619893" MODIFIED="1463911920808" POSITION="right" TEXT="Romans 3:5-28">
+<node CREATED="1463911922592" ID="ID_1586172189" MODIFIED="1463912167785" TEXT="Why should God punish the Jews?">
+<node CREATED="1463912170006" ID="ID_155492644" MODIFIED="1463912186504" TEXT="If Jews are under sin, too, then they receive the same comdemnation as the world"/>
+<node CREATED="1463912187727" ID="ID_1573214684" MODIFIED="1463912199172" TEXT="Sorry Jews, we&apos;re all in the same boat."/>
+</node>
+<node CREATED="1463912222219" ID="ID_740587006" MODIFIED="1463912284351" TEXT="I show the contrast between righteousness and sin (v7-8)">
+<node CREATED="1463912236879" ID="ID_528446270" MODIFIED="1463912245572" TEXT="Aren&apos;t I showing God to be great then?">
+<node CREATED="1463912246907" ID="ID_1139836162" MODIFIED="1463912251245" TEXT="So why should I go to hell"/>
+</node>
+<node CREATED="1463912288881" ID="ID_210075535" MODIFIED="1463912293975" TEXT="Why not just do evil then?"/>
+</node>
+<node CREATED="1463912306636" ID="ID_547968068" MODIFIED="1463912339497" TEXT="Are the Jews any better off? (v.2-18)">
+<node CREATED="1463912317595" ID="ID_674783500" MODIFIED="1463912318845" TEXT="Nope."/>
+</node>
+<node CREATED="1463912339753" ID="ID_1615804473" MODIFIED="1463912488688" TEXT="By the works of the law no human is going to be justified (v.20)">
+<node CREATED="1463912425024" ID="ID_1298839647" MODIFIED="1463912445714" TEXT="It gives you &apos;knowledge&apos; of sin.">
+<node CREATED="1463912449412" ID="ID_1518903570" MODIFIED="1463912459291" TEXT="Just like `knowledge&apos; of God"/>
+</node>
+</node>
+<node CREATED="1463912464210" ID="ID_1830388256" MODIFIED="1463912517863" TEXT="Righteousness in the New Covenant is apart from the law (v.21)">
+<node CREATED="1463912522861" ID="ID_1882896551" MODIFIED="1463912733084" TEXT="Why? God needed to provide the solution to sin (v.26)"/>
+<node CREATED="1463912743357" ID="ID_370654283" MODIFIED="1463912768319" TEXT="Results of this">
+<node CREATED="1463912753055" ID="ID_953214155" MODIFIED="1463912775557" TEXT="No boasting"/>
+<node CREATED="1463912775969" ID="ID_1829697228" MODIFIED="1463912785137" TEXT="Law of faith"/>
+<node CREATED="1463912802629" ID="ID_936182343" MODIFIED="1463912806297" TEXT="Uphold the Law"/>
+</node>
+</node>
+</node>
+<node CREATED="1463912942731" ID="ID_1228091390" MODIFIED="1463912956190" POSITION="left" TEXT="Hebrews 9:15-28">
+<node CREATED="1463912976701" ID="ID_1690487138" MODIFIED="1463912986632" TEXT="Jesus is the mediator of the new covenant (v.15)">
+<node CREATED="1463913063708" ID="ID_1150793483" MODIFIED="1463913082411" TEXT="Blood was necessary (v.22)"/>
+</node>
+<node CREATED="1463913115807" ID="ID_1715410026" MODIFIED="1463913201105" TEXT="Christ died, and took care of sin">
+<node CREATED="1463913155445" ID="ID_1466669498" MODIFIED="1463913165464" TEXT="Just like we die, then are judged (v.27)"/>
+<node CREATED="1463913202218" ID="ID_1937266598" MODIFIED="1463913225429" TEXT="His next coming will not deal with sin, but salvation (v.28)"/>
+</node>
+</node>
+<node CREATED="1463913252911" ID="ID_1385209913" MODIFIED="1463913260176" POSITION="right" TEXT="Romans 7:1-25">
+<node CREATED="1463913378801" ID="ID_1985633776" MODIFIED="1463913391429" TEXT="Good illustration of how we learn to see the need for Christ">
+<node CREATED="1463913414612" ID="ID_479391582" MODIFIED="1463913424803" TEXT="Whether you think it&apos;s before or after being a Christian"/>
+<node CREATED="1463913428061" ID="ID_1548508012" MODIFIED="1463913454546" TEXT="We can see sins for what it is, and be thankful for Christ who redeems."/>
+</node>
+</node>
+<node CREATED="1463913476577" ID="ID_428973193" MODIFIED="1463913488684" POSITION="left" TEXT="Romans 8:1-11">
+<node CREATED="1463913491186" ID="ID_303236316" MODIFIED="1463913505201" TEXT="No condemnation exists for those in Christ"/>
+<node CREATED="1463913514668" ID="ID_815599746" MODIFIED="1463913518056" TEXT="You are set free from sin"/>
+<node CREATED="1463913538191" ID="ID_1374678918" MODIFIED="1463913554529" TEXT="Requirement of the law fulfilled in those who walk not in the flesh, but in the Spirit."/>
+<node CREATED="1463913554987" ID="ID_561248375" MODIFIED="1463913590967" TEXT="You, however, are NOT in the flesh"/>
+<node CREATED="1463913613872" ID="ID_827010540" MODIFIED="1463913618913" TEXT="You MUST have the Spirit of God"/>
+</node>
+</node>
+</map>
